cb29fb4d65a6eaec4bd506388d937892e552156e
Sergey <104456968+Solod-S@users.noreply.github.com>
2023-03-16 21:12:23 +0200

    Main
    - Add & style generic content for main area
    - Create share box
    - Create article template

--- src/components/home/MainSide.js
+++ src/components/home/MainSide.js
@@ -1,7 +1,58 @@
-import { Container } from "./MainSide.styled";
+import { Container, ShareBox, Article, SharedActor } from "./MainSide.styled";
 
+import user from "../../../src/images/user.svg";
+import photo from "../../../src/images/photo-icon.svg";
+import event from "../../../src/images/event.svg";
+import video from "../../../src/images/video.svg";
+import article from "../../../src/images/article.svg";
+import ellipsis from "../../../src/images/ellipsis.svg";
 const MainSide = () => {
-  return <Container>Main side</Container>;
+  return (
+    <Container>
+      <ShareBox>
+        Share
+        <div>
+          <img src={user} alt="user icon" />
+          <button>Start a post</button>
+        </div>
+        <div>
+          <button>
+            <img src={photo} alt="photocard icon" />
+            <span>Photo</span>
+          </button>
+          <button>
+            <img src={video} alt="video icon" />
+            <span>Video</span>
+          </button>
+          <button>
+            <img src={event} alt="event icon" />
+            <span>Event</span>
+          </button>
+          <button>
+            <img src={article} alt="article icon" />
+            <span>Write article</span>
+          </button>
+        </div>
+      </ShareBox>
+      <div>
+        <Article>
+          <SharedActor>
+            <a>
+              <img src={user} alt="user icon" />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+            </a>
+            <button>
+              <img src={ellipsis} alt="ellipsis icon" />
+            </button>
+          </SharedActor>
+        </Article>
+      </div>
+    </Container>
+  );
 };
 
 export default MainSide;
--- src/components/home/MainSide.styled.js
+++ src/components/home/MainSide.styled.js
@@ -4,3 +4,83 @@ import { NavLink } from "react-router-dom";
 export const Container = styled.div`
   grid-area: mainside;
 `;
+
+export const CommondCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgba(0 0 0 /15%), 0 0 0 rgba(0 0 0 /20%);
+`;
+export const ShareBox = styled(CommondCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  background: white;
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0 0.6);
+      font-size: 14px;
+      line-height: 1.5;
+      min-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-size: 60;
+    }
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 0px 16px;
+      img {
+        width: 48px;
+        border-radius: 50%;
+        margin-right: 8px;
+      }
+      button {
+        margin: 4px 0;
+        flex-grow: 1;
+        border-radius: 35px;
+        padding-left: 16px;
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        background-color: white;
+        text-align: left;
+      }
+    }
+    &:nth-child(2) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      padding-bottom: 4px;
+      img {
+        margin: 0 4px 0 -2px;
+      }
+      span {
+        color: #5e5e5e;
+      }
+    }
+  }
+`;
+
+export const Article = styled(CommondCard)`
+  padding: 0;
+  margin: 0 0 8px;
+  overflow: visible;
+`;
+
+export const SharedActor = styled.div`
+  padding-right: 40px;
+  flex-wrap: nowrap;
+  padding: 16px 16px 0;
+  margin-bottom: 8px;
+  align-items: center;
+  display: flex;
+  a {
+    margin-right: 12px;
+  }
+`;
