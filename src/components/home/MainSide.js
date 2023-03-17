54fc4f848fd370009bcde73e52cfeb9e3f45463a
Sergey <104456968+Solod-S@users.noreply.github.com>
2023-03-17 17:44:31 +0200

    Home
    -  continue to create article template

--- src/components/home/MainSide.js
+++ src/components/home/MainSide.js
@@ -1,4 +1,12 @@
-import { Container, ShareBox, Article, SharedActor } from "./MainSide.styled";
+import {
+  Container,
+  ShareBox,
+  Article,
+  SharedActor,
+  Description,
+  SharedImg,
+  SocialCounts,
+} from "./MainSide.styled";
 
 import user from "../../../src/images/user.svg";
 import photo from "../../../src/images/photo-icon.svg";
@@ -6,6 +14,14 @@ import event from "../../../src/images/event.svg";
 import video from "../../../src/images/video.svg";
 import article from "../../../src/images/article.svg";
 import ellipsis from "../../../src/images/ellipsis.svg";
+import exmpPost from "../../../src/images/post_exmpl.jpg";
+import like from "../../../src/images/like.svg";
+import claping from "../../../src/images/claping.svg";
+import likeAction from "../../../src/images/likeAction.svg";
+import commentAction from "../../../src/images/commentAction.svg";
+import reportAction from "../../../src/images/reportAction.svg";
+import sendAction from "../../../src/images/sendAction.svg";
+
 const MainSide = () => {
   return (
     <Container>
@@ -49,6 +65,24 @@ const MainSide = () => {
               <img src={ellipsis} alt="ellipsis icon" />
             </button>
           </SharedActor>
+          <Description>Description</Description>
+          <SharedImg>
+            <a>
+              <img src={exmpPost} alt="shared image" />
+            </a>
+          </SharedImg>
+          <SocialCounts>
+            <li>
+              <button>
+                <img src={like} alt="like icon" />
+                <img src={claping} alt="claping icon" />
+                <span>75</span>
+              </button>
+            </li>
+            <li>
+              <a>2 comments</a>
+            </li>
+          </SocialCounts>
         </Article>
       </div>
     </Container>
--- src/components/home/MainSide.styled.js
+++ src/components/home/MainSide.styled.js
@@ -82,5 +82,84 @@ export const SharedActor = styled.div`
   display: flex;
   a {
     margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+
+    img {
+      width: 48px;
+      height: 48px;
+    }
+    & div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+  }
+`;
+
+export const Description = styled.div`
+  padding: 0 16px;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 14px;
+  text-align: left;
+`;
+
+export const SharedImg = styled.div`
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-color: #f9fafb;
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const SocialCounts = styled.ul`
+  line-height: 1.3;
+  display: flex;
+  align-items: flex-start;
+  overflow: auto;
+  margin: 0 16px;
+  padding: 8px 0;
+  border-bottom: 1px soled #e9e5df;
+  list-style: none;
+  li {
+    margin-right: 5px;
+    font-size: 12px;
+    button {
+      display: flex;
+    }
   }
 `;
